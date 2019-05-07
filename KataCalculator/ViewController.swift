75a0fad1f69624b5fd26b1ab03b607bd640f6c36
Yago de la Fuente <yago.delafuente@quobis.com>
2019-05-07 11:46:29 +0200

    Initial commit

---
+++ KataCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KataCalculator
+//
+//  Created by Yago de la Fuente on 07/05/2019.
+//  Copyright © 2019 Quobis Networks SL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ KataCalculatorTests/KataCalculatorTests.swift
@@ -0,0 +1,26 @@
+//
+//  KataCalculatorTests.swift
+//  KataCalculatorTests
+//
+//  Created by Yago de la Fuente on 07/05/2019.
+//  Copyright © 2019 Quobis Networks SL. All rights reserved.
+//
+
+import XCTest
+@testable import KataCalculator
+
+class KataCalculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {        
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
